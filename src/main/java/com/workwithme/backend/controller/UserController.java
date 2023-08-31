0bfde19c78ebfc6f31f63c8b6e8f5fa85b25c4d7
Merve Doğan <mervdogan@icloud.com>
2023-08-31 19:52:12 +0300

    simple changes

---
+++ src/main/java/com/workwithme/backend/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.workwithme.backend.controller;
+
+import com.workwithme.backend.model.User;
+import com.workwithme.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUSer){
+        return userRepository.save(newUSer);
+    }
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+}
